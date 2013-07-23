f47b63f15d5d37b0f37641b59c239e226501d8c2
poros <antonio.uccio.verardi@gmail.com>
2013-07-24 00:13:28 +0200

    Refined play/pause/stop controls to WiFiSensor thread

--- whereless/Test/WiFi/TestWiFiSensor.cs
+++ whereless/Test/WiFi/TestWiFiSensor.cs
@@ -17,35 +17,38 @@ namespace whereless.Test.WiFi
         [Test(Description = "NativeWiFi example test")]
         public void WiFiSensorTest()
         {
-            var continueThread = new ManualResetEvent(true);
-            var wifiSensor = new WiFiSensor(continueThread);
+            var stopThread = new AutoResetEvent(false);
+            var pauseThread = new AutoResetEvent(false);
+            var playThread = new AutoResetEvent(false);
+            var wifiSensor = new WiFiSensor(stopThread, pauseThread, playThread);
             var wifiDelegate = new ThreadStart(wifiSensor.ExampleMain);
             var wifiThread = new Thread(wifiDelegate);
             wifiThread.Start();
             
             Console.WriteLine("Main Thread go to sleep");
-            Thread.Sleep(8000);
+            Thread.Sleep(16000);
             Console.WriteLine("Main thread is awake");
 
             Console.WriteLine("Pause WiFiSensor");
-            continueThread.Reset();
+            pauseThread.Set();
 
             Console.WriteLine("Main Thread go to sleep");
             Thread.Sleep(8000);
             Console.WriteLine("Main thread is awake");
 
             Console.WriteLine("Restart WiFiSensor");
-            continueThread.Set();
+            playThread.Set();
 
             Console.WriteLine("Main Thread go to sleep");
-            Thread.Sleep(8000);
+            Thread.Sleep(16000);
             Console.WriteLine("Main thread is awake");
 
             Console.WriteLine("Stop WiFiSensor");
-            wifiSensor.StopThread = true;
+            stopThread.Set();
 
             Console.WriteLine("Wait for WiFiSensor");
             wifiThread.Join();
+            Console.WriteLine("WiFiSensor has spontaneously terminated");
         }
     }
 }
--- whereless/WiFi/WiFiSensor.cs
+++ whereless/WiFi/WiFiSensor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Threading;
 using NativeWifi;
 using System;
 using System.Text;
@@ -8,15 +9,12 @@ namespace whereless.WiFi
 {
     public class WiFiSensor
     {
+        // A windows logo compliant wlan interface needs to complete the scan in at most 4 seconds
+        // 300ms given for thread spawning (just to be sure)
+        private readonly int waitTime = 4300;
+        private readonly WaitHandle[] _threadControls = new AutoResetEvent[2];
+        private readonly WaitHandle _play;
         private WlanClient client;
-        private System.Threading.ManualResetEvent _continueThreadEvent;
-        public volatile bool StopThread = false;
-
-        public WiFiSensor(System.Threading.ManualResetEvent continueThread)
-        {
-            client = new WlanClient();
-            this._continueThreadEvent = continueThread;
-        }
 
         // Converts a 802.11 SSID to a string.
         static string GetStringForSsid(Wlan.Dot11Ssid ssid)
@@ -24,56 +22,88 @@ namespace whereless.WiFi
             return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
         }
 
+        public WiFiSensor(WaitHandle stopThread, WaitHandle pauseThread, WaitHandle playThread)
+        {
+            client = new WlanClient();
+            _threadControls[0] = stopThread;
+            _threadControls[1] = pauseThread;
+            _play = playThread;
+        }
+
         public void ExampleMain()
         {
-            for (int i = 0; !StopThread && i < 10; i++)
+            Console.WriteLine("SCAN");
+            var networks = GetNetworksAndScan();
+            foreach (var network in networks)
             {
-                _continueThreadEvent.WaitOne();
-
-                Console.WriteLine("SCAN " + i);
-                // get all wlan intrerfaces
-                foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+                Console.WriteLine("Network SSID: {0} SignalQuality: {1}",
+                                  network.Ssid, network.SignalQuality);
+            }
+            while (true)
+            {
+                int handle = WaitHandle.WaitAny(_threadControls, waitTime);
+                if (handle == 0)
                 {
-                    // only if interface is up (dormant???)
-                    if (wlanIface.NetworkInterface.OperationalStatus == OperationalStatus.Up)
+                    break;
+
+                }
+                else if (handle == 1)
+                {
+                    // Wait until the play event is fired
+                    _play.WaitOne();
+                }
+                else
+                {
+                    Console.WriteLine("SCAN");
+                    networks = GetNetworksAndScan();
+                    foreach (var network in networks)
                     {
-                        // Add the event to track when the wireless connection changes
-                        //wlanIface.WlanNotification +=
-                            //new WlanClient.WlanInterface.WlanNotificationEventHandler(Target);
-
-                        // List all networks
-                        Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0); //0 is a flag
-                        ISet<string> alreadyListedSsids = new HashSet<string>();
-                        foreach (Wlan.WlanAvailableNetwork network in networks)
-                        {
-                            // Trim ad-hoc networks
-                            if (network.dot11BssType != Wlan.Dot11BssType.Independent)
-                            {
-                                string ssid = GetStringForSsid(network.dot11Ssid);
-                                if (!alreadyListedSsids.Contains(ssid))
-                                {
-                                    alreadyListedSsids.Add(ssid);
-                                    Console.WriteLine("Network SSID: {0} SignalQuality: {1}",
-                                                ssid, network.wlanSignalQuality);    
-                                }
-                                
-                            }
-                        }
-
-                        // Ask for a new scan
-                        wlanIface.Scan();
+                        Console.WriteLine("Network SSID: {0} SignalQuality: {1}",
+                                                  network.Ssid, network.SignalQuality);
                     }
 
-                    // A windows logo compliant wlan interface needs to complete the scan in at most 4 seconds
-                    // 300ms given for thread spawning (just to be sure)
-                    System.Threading.Thread.Sleep(4300);
                 }
             }
-            
-            if (StopThread)
+
+            Console.WriteLine("Thread was stopped");
+        }
+
+
+        private IList<IMeasure> GetNetworksAndScan()
+        {
+            var measures = new List<IMeasure>();
+
+            // get all wlan intrerfaces
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
             {
-                Console.WriteLine("Thread was stopped");
+                // only if interface is up (dormant???)
+                if (wlanIface.NetworkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    // Add the event to track when the wireless connection changes
+                    //wlanIface.WlanNotification +=
+                    //new WlanClient.WlanInterface.WlanNotificationEventHandler(Target);
+
+                    // List all networks
+                    Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0); //0 is a flag
+                    ISet<string> alreadyListedSsids = new HashSet<string>();
+                    foreach (Wlan.WlanAvailableNetwork network in networks)
+                    {
+                        // Trim ad-hoc networks
+                        if (network.dot11BssType != Wlan.Dot11BssType.Independent)
+                        {
+                            string ssid = GetStringForSsid(network.dot11Ssid);
+                            if (!alreadyListedSsids.Contains(ssid))
+                            {
+                                alreadyListedSsids.Add(ssid);
+                                measures.Add(new SimpleMeasure(ssid, network.wlanSignalQuality));
+                            }
+                        }
+                    }
+                    // Ask for a new scan
+                    wlanIface.Scan();
+                }
             }
+            return measures;
         }
 
 
